23078deb8dd448b521241604b10034e896a2cb3e
Ben197 <Jabro888us@hotmail.com>
2016-12-19 22:16:23 +0100

    TestWat1 branch

--- TomcatMaven_Test1/src/main/java/n/bp/TestDatabase1.java
+++ TomcatMaven_Test1/src/main/java/n/bp/TestDatabase1.java
@@ -4,9 +4,14 @@ public class TestDatabase1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
 		System.out.println("-------------");
 		System.out.println("TestDatabase2 is aangemaakt");
 		System.out.println("-------------");
+
+		
+
+
 	}
 
 }
