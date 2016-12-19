bbb6d5d1ad51101f4e7fa816965bff082caacc7c
Ben197 <Jabro888us@hotmail.com>
2016-12-19 20:39:45 +0100

    Testdatabase 2 is toegevoegd

--- TomcatMaven_Test1/src/main/java/n/bp/TestDatabase1.java
+++ TomcatMaven_Test1/src/main/java/n/bp/TestDatabase1.java
@@ -4,7 +4,9 @@ public class TestDatabase1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("-------------");
+		System.out.println("TestDatabase2 is aangemaakt");
+		System.out.println("-------------");
 	}
 
 }
