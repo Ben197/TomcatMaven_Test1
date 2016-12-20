5c7e15e42e03810fde3ce632efb53e1c1d293d74
Ben197 <Jabro888us@hotmail.com>
2016-12-20 19:33:51 +0100

    Commit na aanmaken en wijzigen branchtestBP1

--- TomcatMaven_Test1/src/main/java/n/bp/testBP1.java
+++ TomcatMaven_Test1/src/main/java/n/bp/testBP1.java
@@ -6,6 +6,8 @@ public class testBP1 {
 		// TODO Auto-generated method stub
 		System.out.println("krijg een dikke");
 		System.out.println("---------------");
+		System.out.println("branch_testbp---------------");
+		System.out.println("branch_testbp_toegevoegd na branch---------------");
 	}
 
 }
